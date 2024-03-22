4b1769e8faee2b46e5b9252d8b2a05ae53418534
martin <martin.alvarado2002@gmail.com>
2024-03-22 19:33:20 +0100

    [22-336]: Implementing Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    loadChildren: () =>
+      import('./recipes/recipes.module').then((mod) => mod.RecipesModule),
+  },
 
   // { path: 'not-found', component: PageNotFoundComponent },
   // Always the last path
