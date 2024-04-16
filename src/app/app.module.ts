bfc3307e0562d1b6489610aa445f2ba4625e222a
martin <martin.alvarado2002@gmail.com>
2024-04-16 15:49:39 +0200

    [26-395]: Setting Up the NgRx Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { LoggingService } from './logging.service';
-import { StoreModule } from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -16,9 +17,9 @@ import { StoreModule } from '@ngrx/store';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }, {}),
     SharedModule,
     CoreModule,
-    StoreModule.forRoot({}, {}),
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService],
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,16 +1,17 @@
-import { Action } from '@ngrx/store';
-
 import { Ingredient } from '../../shared/ingredient.model';
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as SLA from './shopping-list.actions';
 
 const initialState = {
   ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
 };
 
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(
+  state = initialState,
+  action: SLA.AddIngredient
+) {
   switch (action.type) {
-    case ADD_INGREDIENT:
-      return { ...state, ingredients: [...state.ingredients, action] }; // complete this action
+    case SLA.ADD_INGREDIENT:
+      return { ...state, ingredients: [...state.ingredients, action.payload] }; // complete this action
       break;
 
     default:
