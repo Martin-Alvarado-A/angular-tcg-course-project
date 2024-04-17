2cf522a22c695d122546e3fc42dc590300a86cad
martin <martin.alvarado2002@gmail.com>
2024-04-17 12:45:16 +0200

    [26-406]: Setting Up Auth Reducer & Actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
   user: User;
@@ -8,6 +9,27 @@ const initialState: State = {
   user: null,
 };
 
-export function authReducer(state = initialState, action) {
+export function authReducer(
+  state = initialState,
+  action: AuthActions.AuthActionsType
+) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const payload = action.payload;
+      const user = new User(
+        payload.email,
+        payload.userId,
+        payload.token,
+        payload.expirationDate
+      );
+      return { ...state, user: user };
+
+    case AuthActions.LOGOUT:
+      return { ...state, user: null };
+
+    default:
+      return state;
+  }
+
   return state;
 }
